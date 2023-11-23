1cf63cc8a477e24afbe7efd95709401e1dbec455
vanhieu272 <hoangtranvanhieu@gmail.com>
2023-11-23 20:56:38 +0700

    up bt ss7

---
+++ ss05_ss06_ORM_JPA_Hibernate/bai_tap/Blog-spring-boot/src/main/java/com/example/blogspringboot/repository/ICategoryRepository.java
@@ -0,0 +1,2 @@
+package com.example.blogspringboot.repository;public interface ICategoryRepository {
+}
---
+++ ss05_ss06_ORM_JPA_Hibernate/bai_tap/Blog-spring-boot/src/main/java/com/example/blogspringboot/service/ICategoryService.java
@@ -0,0 +1,2 @@
+package com.example.blogspringboot.service;public interface ICategoryService {
+}
